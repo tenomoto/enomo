ff662b9c40684b76955f5716debd85ee2d5a642d
Takeshi Enomoto <eno@jamstec.go.jp>
2010-04-23 12:12:23 +0900

    modified vectrans_module.f90 to use shtrans_module.f90

--- vectrans_module.f90
+++ vectrans_module.f90
@@ -1,304 +1,283 @@
 module vectrans_module
+  use shtrans_module, only: shtrans_verbose, shtrans_isinit, shtrans_init, & 
+    shtrans_transf, shtrans_transb, shtrans_hoskinsfilter, shtrans_truncate
   use kind_module, only: dp, i4b
   private
 
-  real(kind=dp), private :: kfil
-  real(kind=dp), dimension(:), allocatable, private :: wvts, wvhags, wshsgs,work
-  real(kind=dp), dimension(:,:,:), allocatable, private :: v, w, br, bi, cr, ci
-  integer(kind=i4b), private :: &
-    nlon, nlat, nt, ntrunc, lvhags, lwvts, lshsgs, mdab, ndab, idvw, jdvw, ierror, lwork
+  logical, public :: vectrans_verbose = .false.
 
-  public vectrans_init, vectrans_clean, vectrans_dlat, vectrans_uv2vrdv
+  integer(kind=i4b), parameter, private :: ityp = 0, isym = 0
+  real(kind=dp), dimension(:), allocatable, private :: wvts, wvhags, wvhsgs, work
+  real(kind=dp), dimension(:,:,:), allocatable, private :: v, w, br, bi, cr, ci, a, b, g
+  integer(kind=i4b), private :: &
+    nlon, nlat, ntrunc, mdab, ndab, idvw, jdvw
+  logical, private :: lyrev
+
+  public :: vectrans_init, vectrans_clean, vectrans_dlat, vectrans_uv2vrdv, vectrans_gradient
+  private :: checkerror, checkval
 
 contains
 
-  subroutine vectrans_init(nx,ny,n)
+  subroutine vectrans_init(nx,ny,nt,np2sp)
     implicit none
 
-    integer(kind=i4b), intent(in) :: nx, ny, n
+    integer(kind=i4b), intent(in) :: nx, ny, nt
+    logical, optional, intent(in) :: np2sp
 
     real(kind=dp), dimension(:), allocatable :: dwork
-    integer(kind=i4b) :: l1, l2, ldwork
+    integer(kind=i4b) :: l1, l2, ldwork, lvhags, lvhsgs, lwvts, lwork, ierror
 
     nlon = nx
     nlat = ny
-    nt = n
+    ntrunc = ceiling(nlon/3.0)-1
+    lyrev = .true.
+    if (present(np2sp).and.np2sp) then
+      lyrev = .false.
+    end if
+    if (vectrans_verbose) then
+      print *, "nlat=", nlat, " nlon=", nlon, " ntrunc=", ntrunc, &
+        " nt=", nt, " lyrev=", lyrev
+    end if
 
+! spherepack uses colatitudes (colat = pi/2 - lat)
+!   - colat = pi/2 - lat, colat = 0 at NP
+!   - data is stored from NP->SP
+!   - meridional wind is reveresed -v
+!   - merional derivative is reversed -d/dtheta
+
+! init analysis
     l1 = min0(nlat,nlon/2) 
     l2 = nlat/2
     lvhags = l1*l2*(nlat+nlat-l1+1)+nlon+15
     ldwork = (3*nlat*(nlat+3)+2)/2
     allocate(wvhags(lvhags),dwork(ldwork))
     call vhagsi(nlat,nlon,wvhags,lvhags,dwork,ldwork,ierror)
-    if (ierror/=0) then
-      print *, "*** error in vhagsi: ", ierror
-      stop
-!    else
-!      print *, "nlat,nlon:", nlat, nlon
-!      print *, "wvhags:", minval(wvhags), maxval(wvhags), size(wvhags), lvhags
-!      print *, "dwork:", size(dwork)
-    end if
+    call checkerror(ierror,"vhagsi")
+
+! init synthesis
+    lvhsgs = l1*l2*(nlat+nlat-l1+1)+nlon+15+2*nlat
+    allocate(wvhsgs(lvhsgs))
+    call vhsgsi(nlat,nlon,wvhsgs,lvhsgs,dwork,ldwork,ierror)
+    call checkerror(ierror,"vhsgsi")
     deallocate(dwork)
 
+! init colatitude derivatives
     lwvts  = l1*l2*(nlat+nlat-l1+1)+nlon+15
     ldwork = 3*nlat+2
     lwork = 3*(max0(l1-2,0)*(nlat+nlat-l1-1))/2+(5*l2+2)*nlat
     allocate(wvts(lwvts),work(lwork),dwork(ldwork))
     call vtsgsi(nlat,nlon,wvts,lwvts,work,lwork,dwork,ldwork,ierror)
-    if (ierror/=0) then
-      print *, "*** error in vtsgsi: ", ierror
-      stop
-!    else
-!      print *, "wvts:", minval(wvts), maxval(wvts)
-    end if
+    call checkerror(ierror,"vtsgsi")
     deallocate(dwork,work)
-
-    lshsgs = nlat*(3*(l1+l2)-2)+(l1-1)*(l2*(2*nlat-l1)-3*l1)/2+nlon+15
-    lwork = 4*nlat*(nlat+2)+2
-    ldwork = nlat*(nlat+4)
-    allocate(wshsgs(lshsgs),work(lwork),dwork(ldwork))
-    call shsgsi(nlat,nlon,wshsgs,lshsgs,work,lwork,dwork,ldwork,ierror)
-    if (ierror/=0) then
-      print *, "*** error in shsgsi: ", ierror
-      stop
-    end if
 
     mdab = min0(nlat,nlon/2)
     ndab = nlat
     allocate(br(mdab,ndab,nt),bi(mdab,ndab,nt),cr(mdab,ndab,nt),ci(mdab,ndab,nt))
+    allocate(g(nlat,nlon,nt),a(mdab,ndab,nt),b(mdab,ndab,nt))
 
     idvw = nlat
     jdvw = nlon
     allocate(v(idvw,jdvw,nt), w(idvw,jdvw,nt))
 
-    deallocate(dwork,work)
+! vhags, vtsgs=vhsgs
     lwork = max(3*nlat*(nlat+1)+2,(2*nt+1)*nlat*nlon)
-    lwork = max(lwork,(2*nt+1)*nlat*nlon)
+! vrtgs
+    lwork = max(lwork,nlat*((nt+1)*nlon+2*nt*l1+1))
+! divgs
+    l1 = min0(nlat,(nlon+2)/2)
     lwork = max(lwork,nlat*((nt+1)*nlon+2*nt*l1+1))
     allocate(work(lwork))
-
-! Hoskins filter
-    ntrunc = nlon/3
-    if (mod(nlon,3)/=0) then
-      ntrunc = ntrunc - 1
-    end if
-    kfil = -log(0.1_dp)/(ntrunc*(ntrunc+1))**2
 
   end subroutine vectrans_init
 
   subroutine vectrans_clean
+    use shtrans_module, only: shtrans_clean
     implicit none
 
-    deallocate(wvhags,wvts,work,br,bi,cr,ci,v,w)
+    deallocate(wvhags,wvhsgs,wvts,work,br,bi,cr,ci,v,w,g,a,b)
+    if (shtrans_isinit) then
+      call shtrans_clean()
+    end if
 
   end subroutine vectrans_clean
 
-  subroutine vectrans_dlat(ui,vi,uy,vy,np2sp,lfil)
+  subroutine vectrans_dlat(ui,vi,uy,vy,lfil)
     implicit none
 
     real(kind=dp), dimension(:,:,:), intent(in) :: ui, vi
     real(kind=dp), dimension(:,:,:), intent(inout) :: uy, vy
-    logical, optional, intent(in) :: np2sp, lfil
-
-    integer(kind=i4b), parameter :: ityp = 0
+    logical, optional, intent(in) :: lfil
 
     real(kind=dp) :: fil
-    integer(kind=i4b) :: i, j, jr, k, n, m
+    integer(kind=i4b) :: i, j, jr, k, n, m, nt, ierror
 
-! spherepack uses colatitudes (colat = pi/2 - lat)
-!   - colat = pi/2 - lat, colat = 0 at NP
-!   - data is stored from NP->SP
-!   - meridional wind is reveresed -v
-!   - merional derivative is reversed -d/dtheta
-    if (present(np2sp).and.np2sp) then
-      do k=1, nt
-        do j=1, nlat
-          v(j,1:nlon,k) = -vi(1:nlon,j,k)
-          w(j,1:nlon,k) =  ui(1:nlon,j,k)
-        end do
-      end do
-    else
-      do k=1, nt
-        do j=1, nlat
-          jr = nlat - j +1
-          v(jr,1:nlon,k) = -vi(1:nlon,j,k)
-          w(jr,1:nlon,k) =  ui(1:nlon,j,k)
-        end do
-      end do
-    end if
-    call vhags(nlat,nlon,ityp,nt,v,w,idvw,jdvw,br,bi,cr,ci, &
-               mdab,ndab,wvhags,lvhags,work,lwork,ierror)
-    if (ierror/=0) then
-      print *, "*** error in vhags: ", ierror
-      stop
-    end if
+    nt = size(ui,3)
+    call shtrans_transf(vi,v,lyrev)
+    call shtrans_transf(ui,w,lyrev)
+    v(:,:,:) = -v(:,:,:)
+    call vhags(nlat,nlon,isym,nt,v,w,idvw,jdvw,br,bi,cr,ci, &
+               mdab,ndab,wvhags,size(wvhags),work,size(work),ierror)
+    call checkerror(ierror,"vhags")
 
-! triangular trunction
-    br(ntrunc+2:mdab,ntrunc+2:ndab,:) = 0.0_dp
-    bi(ntrunc+2:mdab,ntrunc+2:ndab,:) = 0.0_dp
-    cr(ntrunc+2:mdab,ntrunc+2:ndab,:) = 0.0_dp
-    ci(ntrunc+2:mdab,ntrunc+2:ndab,:) = 0.0_dp
+    call shtrans_truncate(br,ntrunc)
+    call shtrans_truncate(bi,ntrunc)
+    call shtrans_truncate(cr,ntrunc)
+    call shtrans_truncate(ci,ntrunc)
 
     if (present(lfil).and.lfil) then
-      do n=0, ntrunc
-        fil = exp(-kfil*(n*n*(n+1)*(n+1)))
-        do m=0, n
-          br(m+1,n+1,:) = fil*br(m+1,n+1,:)
-          bi(m+1,n+1,:) = fil*bi(m+1,n+1,:)
-          cr(m+1,n+1,:) = fil*cr(m+1,n+1,:)
-          ci(m+1,n+1,:) = fil*ci(m+1,n+1,:)
-        end do
-      end do
+      call shtrans_hoskinsfilter(br,ntrunc)
+      call shtrans_hoskinsfilter(bi,ntrunc)
+      call shtrans_hoskinsfilter(cr,ntrunc)
+      call shtrans_hoskinsfilter(ci,ntrunc)
     end if
 
-!! debug
-!    print *, "v:", minval(v), maxval(v)
-!    print *, "w:", minval(w), maxval(w)
-!    print *, "wvhags:", minval(wvhags), maxval(wvhags)
-!    print *, "br:", minval(br), maxval(br)
-!    print *, "bi:", minval(bi), maxval(bi)
-!    print *, "cr:", minval(cr), maxval(cr)
-!    print *, "ci:", minval(ci), maxval(ci)
-!stop
+    call vtsgs(nlat,nlon,isym,nt,v,w,idvw,jdvw,br,bi,cr,ci, &
+               mdab,ndab,wvts,size(wvts),work,size(work),ierror)
+    call checkerror(ierror,"vtsgs")
+    call shtrans_transb(w,uy,lyrev)
+    call shtrans_transb(v,vy,lyrev)
 
-    call vtsgs(nlat,nlon,ityp,nt,v,w,idvw,jdvw,br,bi,cr,ci, &
-               mdab,ndab,wvts,lwvts,work,lwork,ierror)
-    if (ierror==0) then
-      if (present(np2sp).and.np2sp) then
-        do k=1, nt
-          do j=1, nlat
-            vy(1:nlon,j,k) =  v(j,1:nlon,k)
-            uy(1:nlon,j,k) = -w(j,1:nlon,k)
-          end do
-        end do
-      else
-        do k=1, nt
-          do j=1, nlat
-            jr = nlat - j + 1
-            vy(1:nlon,jr,k) =  v(j,1:nlon,k)
-            uy(1:nlon,jr,k) = -w(j,1:nlon,k)
-          end do
-        end do
-      end if
-    else
-      print *, "*** error in vtsgs: ", ierror
-      stop
+    if (vectrans_verbose) then
+      call checkval(ui,"ui")
+      call checkval(vi,"vi")
+      call checkval(br,"br")
+      call checkval(bi,"bi")
+      call checkval(cr,"cr")
+      call checkval(ci,"ci")
+      call checkval(uy,"uy")
+      call checkval(vy,"vy")
     end if
 
   end subroutine vectrans_dlat
 
-  subroutine vectrans_uv2vrdv(ui,vi,vr,dv,np2sp,lfil)
+  subroutine vectrans_uv2vrdv(ui,vi,vr,dv,lfil)
+    use shtrans_module, only: wshsgs=>shtrans_wshsgs
     implicit none
 
     real(kind=dp), dimension(:,:,:), intent(in) :: ui, vi
     real(kind=dp), dimension(:,:,:), intent(inout) :: vr, dv
-    logical, optional, intent(in) :: np2sp,lfil
-
-    integer(kind=i4b), parameter :: ityp = 0
+    logical, optional, intent(in) :: lfil
 
     real(kind=dp) :: fil
-    integer(kind=i4b) :: i, j, jr, k, n, m
-
-! spherepack uses colatitudes (colat = pi/2 - lat)
-!   - colat = pi/2 - lat, colat = 0 at NP
-!   - data is stored from NP->SP
-!   - meridional wind is reveresed -v
-!   - merional derivative is reversed -d/dtheta
-    if (present(np2sp).and.np2sp) then
-      do k=1, nt
-        do j=1, nlat
-          v(j,1:nlon,k) = -vi(1:nlon,j,k)
-          w(j,1:nlon,k) =  ui(1:nlon,j,k)
-        end do
-      end do
-    else
-      do k=1, nt
-        do j=1, nlat
-          jr = nlat - j +1
-          v(jr,1:nlon,k) = -vi(1:nlon,j,k)
-          w(jr,1:nlon,k) =  ui(1:nlon,j,k)
-        end do
-      end do
+    integer(kind=i4b) :: i, j, jr, k, n, m, nx, ny, nt, ierror
+    
+    nx = size(ui,1)
+    ny = size(ui,2)
+    nt = size(ui,3)
+    if (.not.shtrans_isinit) then
+      shtrans_verbose = vectrans_verbose
+      call shtrans_init(nx,ny,nt,.not.lyrev)
     end if
+
+    call shtrans_transf(ui,w,lyrev)
+    call shtrans_transf(vi,v,lyrev)
+    v(:,:,:) = -v(:,:,:)
     call vhags(nlat,nlon,ityp,nt,v,w,idvw,jdvw,br,bi,cr,ci, &
-               mdab,ndab,wvhags,lvhags,work,lwork,ierror)
-    if (ierror/=0) then
-      print *, "*** error in vhags: ", ierror
-      stop
-    end if
+               mdab,ndab,wvhags,size(wvhags),work,size(work),ierror)
+    call checkerror(ierror,"vhags")
 
-! triangular trunction
-    br(ntrunc+2:mdab,ntrunc+2:ndab,:) = 0.0_dp
-    bi(ntrunc+2:mdab,ntrunc+2:ndab,:) = 0.0_dp
-    cr(ntrunc+2:mdab,ntrunc+2:ndab,:) = 0.0_dp
-    ci(ntrunc+2:mdab,ntrunc+2:ndab,:) = 0.0_dp
+    call shtrans_truncate(br,ntrunc)
+    call shtrans_truncate(bi,ntrunc)
+    call shtrans_truncate(cr,ntrunc)
+    call shtrans_truncate(ci,ntrunc)
 
     if (present(lfil).and.lfil) then
-      do n=0, ntrunc
-        fil = exp(-kfil*(n*n*(n+1)*(n+1)))
-        do m=0, n
-          br(m+1,n+1,:) = fil*br(m+1,n+1,:)
-          bi(m+1,n+1,:) = fil*bi(m+1,n+1,:)
-          cr(m+1,n+1,:) = fil*cr(m+1,n+1,:)
-          ci(m+1,n+1,:) = fil*ci(m+1,n+1,:)
-        end do
-      end do
+      call shtrans_hoskinsfilter(br,ntrunc)
+      call shtrans_hoskinsfilter(bi,ntrunc)
+      call shtrans_hoskinsfilter(cr,ntrunc)
+      call shtrans_hoskinsfilter(ci,ntrunc)
     end if
 
-!! debug
-!    print *, "v:", minval(v), maxval(v)
-!    print *, "w:", minval(w), maxval(w)
-!    print *, "wvhags:", minval(wvhags), maxval(wvhags)
-!    print *, "br:", minval(br), maxval(br)
-!    print *, "bi:", minval(bi), maxval(bi)
-!    print *, "cr:", minval(cr), maxval(cr)
-!    print *, "ci:", minval(ci), maxval(ci)
-!stop
+    call vrtgs(nlat,nlon,isym,nt,v,idvw,jdvw,cr,ci, &
+               mdab,ndab,wshsgs,size(wshsgs),work,size(work),ierror)
+    call checkerror(ierror,"vrtgs")
+    call shtrans_transb(v,vr,lyrev)
+    call divgs(nlat,nlon,isym,nt,w,idvw,jdvw,br,bi, &
+               mdab,ndab,wshsgs,size(wshsgs),work,size(work),ierror)
+    call checkerror(ierror,"divgs")
+    call shtrans_transb(w,dv,lyrev)
 
-    call vrtgs(nlat,nlon,ityp,nt,v,idvw,jdvw,cr,ci, &
-               mdab,ndab,wshsgs,lshsgs,work,lwork,ierror)
-    if (ierror==0) then
-      if (present(np2sp).and.np2sp) then
-        do k=1, nt
-          do j=1, nlat
-            vr(1:nlon,j,k) =  v(j,1:nlon,k)
-          end do
-        end do
-      else
-        do k=1, nt
-          do j=1, nlat
-            jr = nlat - j + 1
-            vr(1:nlon,jr,k) =  v(j,1:nlon,k)
-          end do
-        end do
-      end if
-    else
-      print *, "*** error in vrtgs : ", ierror
-      stop
-    end if
-    call divgs(nlat,nlon,ityp,nt,w,idvw,jdvw,br,bi, &
-               mdab,ndab,wshsgs,lshsgs,work,lwork,ierror)
-            dv(1:nlon,jr,k) = -w(j,1:nlon,k)
-    if (ierror==0) then
-      if (present(np2sp).and.np2sp) then
-        do k=1, nt
-          do j=1, nlat
-            dv(1:nlon,j,k) =  w(j,1:nlon,k)
-          end do
-        end do
-      else
-        do k=1, nt
-          do j=1, nlat
-            jr = nlat - j + 1
-            dv(1:nlon,jr,k) =  w(j,1:nlon,k)
-          end do
-        end do
-      end if
-    else
-      print *, "*** error in divgs: ", ierror
-      stop
+    if (vectrans_verbose) then
+      call checkval(ui,"ui")
+      call checkval(vi,"vi")
+      call checkval(br,"br")
+      call checkval(bi,"bi")
+      call checkval(cr,"cr")
+      call checkval(ci,"ci")
+      call checkval(vr,"vr")
+      call checkval(dv,"dv")
     end if
 
   end subroutine vectrans_uv2vrdv
+
+  subroutine vectrans_gradient(gin,gx,gy,lfil)
+    use shtrans_module, only: wshags=>shtrans_wshags
+    implicit none
+
+    real(kind=dp), dimension(:,:,:), intent(in) :: gin
+    real(kind=dp), dimension(:,:,:), intent(inout) :: gx, gy
+    logical, optional, intent(in) :: lfil
+
+    integer(kind=i4b) :: nx, ny, nt, ierror
+
+    nx = size(gin,1)
+    ny = size(gin,2)
+    nt = size(gin,3)
+    if (.not.shtrans_isinit) then
+      shtrans_verbose = vectrans_verbose
+      call shtrans_init(nx,ny,nt,.not.lyrev)
+    end if
+    call shtrans_transf(gin,g,lyrev)
+    call shags(nlat,nlon,isym,nt,g,idvw,jdvw,a,b,mdab,ndab, &
+               wshags,size(wshags),work,size(work),ierror)
+    call checkerror(ierror,"shags")
+
+    call shtrans_truncate(a,ntrunc)
+    call shtrans_truncate(b,ntrunc)
+    if (present(lfil).and.lfil) then
+      call shtrans_hoskinsfilter(a,ntrunc)
+      call shtrans_hoskinsfilter(b,ntrunc)
+    end if
+    call gradgs(nlat,nlon,isym,nt,v,w,idvw,jdvw,a,b,mdab,ndab, &
+                wvhsgs,size(wvhsgs),work,size(work),ierror)
+    call checkerror(ierror,"gradgs")
+
+    v(:,:,:) = -v(:,:,:)
+    call shtrans_transb(v,gy,lyrev)
+    call shtrans_transb(w,gx,lyrev)
+
+    if (vectrans_verbose) then
+      call checkval(gin,"gin")
+      call checkval(a,"a")
+      call checkval(b,"b")
+      call checkval(gx,"gx")
+      call checkval(gy,"gy")
+    end if
+
+  end subroutine vectrans_gradient
+
+  subroutine checkerror(ierror,name)
+    implicit none
+
+    integer(kind=i4b), intent(in) :: ierror
+    character(len=*), intent(in) :: name
+
+    if (ierror/=0) then
+      print *, "*** error in ",name,": ", ierror
+      stop
+    end if
+
+  end subroutine checkerror
+
+  subroutine checkval(x,name)
+    implicit none
+
+    real(kind=dp), dimension(:,:,:) :: x
+    character(len=*), intent(in) :: name
+
+    print *, name, " min=", minval(x), " at ", minloc(x), &
+             name, " max=", maxval(x), " at ", maxloc(x)
+
+  end subroutine checkval
 
 end module vectrans_module
